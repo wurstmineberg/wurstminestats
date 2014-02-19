aa9ac3e84be5863e83e3ac9440f7254da03790bf
Jemus42 <jemus42@gmail.com>
2014-02-19 01:40:22 +0100

    Finally fixed the sorting and IDs

--- Playerstats.R
+++ Playerstats.R
@@ -11,20 +11,23 @@ playerstats <- fromJSON("http://api.wurstmineberg.de/server/playerstats/general.
 names(playerstats) <- sub("stat.","",names(playerstats))
 playerstats[playerstats == "NULL"] <- "0"
 playerTemp <- names(playerstats[,1])
+## Add "none" category to people$status for easier matching
+people$status[is.na(people$status)] <- "none"
 ## Getting rid of the nested list stuff
 ## This took me so long, please don't even ask me about it.
 for(i in (1:23)){
   playerstats[i] <- unlist(playerstats[i], use.names=F)
 } 
 rm(i)
+## Getting rid of NAs and assuming 0
 playerstats[playerstats == NA] <- 0
+## Numericizzle
 playerstats <- as.data.frame(mapply(as.numeric,playerstats))
 ## Sorting according to people.json
-playerstats$player <- playerTemp
-playerstats <- playerstats[match(people$minecraft, playerstats$player),]
-#sortList <- match(people$minecraft,playerstats$player)[!is.na(match(people$minecraft,playerstats$player))]
-#playerstats <- playerstats[sortList,]
-playerstats$joinDate <- people$join_date
+playerstats$player <- playerTemp; rm(playerTemp);
+playerstats <- playerstats[match(people$minecraft[people$status != "former"], playerstats$player),]
+
+playerstats$joinDate <- people$join_date[people$status != "former"]
 
 rownames(playerstats) <- NULL
 #playerstats <- playerstats[playerstats == NA] <- "0"
@@ -33,9 +36,12 @@ playerstats <- playerstats[c(ncol(playerstats)-1,
                  ncol(playerstats),
                  1:(ncol(playerstats)-2))]
 
-playerstats$player <- people$id
+playerstats$player <- people$id[people$status != "former"]
+
 playerstats$player <- factor(playerstats$player)
 
+## Getting rid of NAs and assuming 0 (again. Don't ask.)
+playerstats[playerstats == NA] <- 0
 
 ## plots
 ggplot(data=playerstats, aes(x=player, y=deaths)) + 
@@ -65,3 +71,4 @@ ggplot(playerstats, aes(x = playOneMinute,
   ggsave("Plots/Deaths_OnlineTime.png")
 
 cor(playerstats$deaths,playerstats$playOneMinute)^2
+
