35a6cd6d855e01d6ebf8398d56d989dce0167ebd
Jemus42 <jemus42@gmail.com>
2014-02-21 00:10:17 +0100

    Regenerate stuff to honor @viirus42's rise from the Karteileichen

--- Playerstats.R
+++ Playerstats.R
@@ -18,8 +18,15 @@ ggplot(data=playerstats, aes(x=player, y=deaths)) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
   ggsave("Plots/Deaths.png")
 
-ggplot(data=playerstats, aes(x=player, y=(walkOneCm/1000000))) + 
+ggplot(data=playerstats, aes(x=player, y=playOneMinute)) + 
   geom_bar(colour="black", width=.7, stat="identity") + 
+  xlab("Player") + ylab("Online time (hours (real time))") +
+  ggtitle("Online time of players on Wurstmineberg") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+ggsave("Plots/PlayTime.png")
+
+ggplot(data=playerstats, aes(x=player, y=(walkOneCm/1000000))) + 
+  geom_bar(width=.7, stat="identity") + 
   xlab("Player") + ylab("Distance (km)") +
   ggtitle("Distance walked on Wurstmineberg") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
@@ -29,12 +36,12 @@ ggplot(playerstats, aes(x = playTimeHours, y = deaths)) +
   geom_point(aes(colour=player), group=1) + 
  # geom_smooth(method = lm) + 
   ylab("Deaths") + xlab("Online time (hours (real time))") +
-  ggtitle("Deaths vs. Online time") 
-  #playerTheme +
-  #scale_colour_discrete(name = "Name") 
+  ggtitle("Deaths vs. Online time") +
+  playerTheme +
+  scale_colour_discrete(name = "Name") 
 ggsave("Plots/Deaths_OnlineTime.png")
 
-# cor(playerstats$deaths,playerstats$playOneMinute)^2
+#cor(playerstats$deaths,playerstats$playOneMinute)^2
 
 # Mob kills vs play time
 ggplot(playerstats, aes(x=playTimeHours, y=mobKills, label=player)) + 
@@ -48,9 +55,7 @@ ggplot(playerstats, aes(x=playTimeHours, y=mobKills, label=player)) +
 ggsave("Plots/MobKills_OnlineTime.png")
 
 # Distance walked per online time
-ggplot(playerstats, aes(x = playTimeHours, 
-                        y = (walkOneCm/1000),
-                        label=player)) + 
+ggplot(playerstats, aes(x = playTimeHours,  y = (walkOneCm/1000000), label=player)) + 
   geom_point(aes(colour=player, group=1)) + 
  # geom_smooth(method = lm) + 
   ylab("Distance walked (km)") + xlab("Online time (hours (real time)))") +
@@ -61,22 +66,23 @@ ggsave("Plots/DistanceWalked_OnlineTime.png")
 
 # Server growth
 ggplot(playerstats, aes(x = joinDate, y = number, label = player)) + 
-  geom_smooth(method = loess, se=F) + 
-  geom_text(size=2, hjust=-.2, vjust=.4) +
-  geom_point(aes(group=1)) + 
+  #geom_smooth(method = loess, se=F) + 
+  geom_text(size=2, vjust=-.7, hjust=-.2) +
+  geom_point() + 
   ylab("Whitelist count") + xlab("Date") +
   ggtitle("Wurstmineberg Server Growth") +
   playerTheme +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  #ylim(-1,30) +
   scale_colour_discrete(name = "Name") +
   scale_x_datetime(labels = date_format("%y-%m-%d"),
                    breaks = date_breaks("month"));
 ggsave("Plots/WhitelistGrowth.png")
+cor(playerstats$serverAge,playerstats$number, method="spearman")
+
 
 
 # Play time vs server age
-ggplot(playerstats, aes(x = serverAge, y = playTimeHours, label=number)) +
+ggplot(playerstats, aes(x = serverAge, y = playTimeHours, label=player)) +
   geom_text(size=2, hjust=.5, vjust=-1) +
   geom_point(aes(colour=joinStatus, group=1)) + 
   ylab("Time spent on server (hours)") + xlab("Age on server (days)") +
