35e8a0a99d9a018f06d5d2c15e38c99a1213c4ad
Jemus42 <jemus42@gmail.com>
2014-02-22 06:55:30 +0100

    Reformatting, cleanup, yadda yadda

--- dataPrep.R
+++ dataPrep.R
@@ -1,13 +1,11 @@
 # Datapreparations
 
-# Getting player stats
 library(RCurl)
 library(jsonlite)
 library(ggplot2)
 library(scales)     # For datetime scales on plots
 library(gridExtra)  # For annotations outside of plot ## TODO
 
-## Getting some data for furthe use
 # Get people.json for player id and join dates
 people <- fromJSON("http://wurstmineberg.de/assets/serverstatus/people.json")
 ## Get player stats from wurstmineberg API
@@ -16,57 +14,67 @@ playerstats <- fromJSON("http://api.wurstmineberg.de/server/playerstats/general.
 # A little cleanup
 names(playerstats) <- sub("stat.","",names(playerstats))
 playerstats[playerstats == "NULL"] <- "0"
+
+# Extract player names to separate variable
 playerTemp <- names(playerstats[,1])
 
-## Add "organic" category to people$status for easier matching
+## Add category to people$status for easier matching
 people$status[is.na(people$status)] <- "later"
 
 ## Getting rid of the nested list stuff
-## This took me so long, please don't even ask me about it.
-for(i in (1:(ncol(playerstats)))){
+# This took me so long, please don't even ask me about it.
+for(i in (1:(ncol(playerstats)))) {
   playerstats[i] <- unlist(playerstats[i], use.names=F)
-} 
-rm(i)
-## Getting rid of NAs and assuming 0
+}; rm(i);
+
+# Getting rid of NAs and assuming 0
 playerstats[playerstats == NA] <- 0
-## Numericizzle
+
+# Numericizzle
 playerstats <- as.data.frame(mapply(as.numeric,playerstats))
+
 ## Sorting according to people.json
 playerstats$player <- playerTemp; rm(playerTemp);
-## Crucial part where we enhance the original list by matching with people.json
+
+# Crucial part where we enhance the original list by matching with people.json
 playerstats <- playerstats[match(people$minecraft[people$status != "former"], playerstats$player),]
+
 ## Get joinDate from people.json, excluding former members
 playerstats$joinDate <- people$join_date[people$status != "former"]
-## Get rid of unnecessary rownames column (we'll get an index later)
+
+# Get rid of unnecessary rownames column (we'll get an index later)
 rownames(playerstats) <- NULL
+
 ## Resort columns to get name and joinDate first. Optional!
 playerstats <- playerstats[c(ncol(playerstats)-1,
                              ncol(playerstats),
                              1:(ncol(playerstats)-2))]
+
 ## Convert player names to people.json-IDs
 playerstats$player <- people$id[people$status != "former"]
-## Convert to factors with appropriate levels
+
+# Convert to factors with appropriate levels
 playerstats$player <- factor(playerstats$player, 
                              levels=playerstats$player)
 
 ## Getting rid of NAs and assuming 0 (again. Don't ask.)
 playerstats[is.na(playerstats)] <- 0
-## Just to get a numeric ID to have an easy index and player number
+
+# Just to get a numeric ID to have an easy index and player number
 playerstats$number <- (1:(nrow(playerstats)))
 
 ## Give people status values because lol
 playerstats$joinStatus <- as.factor(people$status[people$status != "former"])
 
-## In case of missing join date, apply NA
+# In case of missing join date, apply NA
 playerstats$joinDate[playerstats$joinDate == 0] <- NA
 
-## Convert joinDate to POSIXct because time
+# Convert joinDate to POSIXct because time
 playerstats$joinDate <- as.POSIXct(playerstats$joinDate, 
                                    origin="1970-01-01")
 
-# Convert play time to real time hours
+## Convert play time to real time hours
 playerstats$playOneHour <- (playerstats$playOneMinute/20/60/60)
-
 
 ## Define server birth and server age times
 
@@ -90,12 +98,8 @@ for(i in 1:nrow(playerstats)){
 }; rm(i);
 
 ## Get a vector of the age gaps starting from player[1]
-#c(0,playerstats$serverAge[1:25] - playerstats$serverAge[2:26])
+inviteGaps <- c(0,playerstats$serverAge[1:25] - playerstats$serverAge[2:26])
+mean(inviteGaps)
 
-# Wirte datasets to file for ze easy access
+## Write dataset to file for ze easy access
 write.csv(playerstats, "playerstats.csv")
-
-#people <- subset(people, select=-irc)
-#people <- subset(people, select=-options)
-#people <- subset(people, select=-description)
-#people <- subset(people, select=-fav_item)
