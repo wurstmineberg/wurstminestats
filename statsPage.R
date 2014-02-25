12b374c19d4c26c72d2850222b35ec2c3f1aeb86
Jemus42 <jemus42@gmail.com>
2014-02-25 20:46:35 +0100

    Add remaining weighted charts

--- statsPage.R
+++ statsPage.R
@@ -149,7 +149,91 @@ ggsave(plot=p, file="Plots/statspage/treasureFished.png", height=plotHeight, wid
 
 rm(p)
 
-### Weighted by hours played
+###################### Weighted by hours played
+
+# Animals Bred
+p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player,animalsBred/playOneHour), y=animalsBred/playOneHour)) + 
+  barChart + legendTitle + coord_flip() +
+  xLable + labs(y="Number of Animals per Hour (real time)", title="Animals Bred")
+ggsave(plot=p,file="Plots/statspage/animalsBred_weighted.png", height=plotHeight, width=plotWidth)
+
+# Damage Dealt
+p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player,damageDealt/playOneHour), y=(damageDealt/2000)/playOneHour)) + 
+  barChart + legendTitle + coord_flip() +
+  xLable + labs(y="Hearts (thousands) per Hour (real time)", title="Damage Dealt")
+ggsave(plot=p, file="Plots/statspage/damageDealt_weighted.png", height=plotHeight, width=plotWidth)
+
+# Damage Taken
+p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player,damageTaken/playOneHour), y=(damageTaken/2000)/playOneHour)) + 
+  barChart + legendTitle + coord_flip() +
+  xLable + labs(y="Hearts (thousands) per Hour (real time)", title="Damage Taken")
+ggsave(plot=p, file="Plots/statspage/damageTaken_weighted.png", height=plotHeight, width=plotWidth)
+
+# Distance by Boat
+p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player,boatOneCm/playOneHour), y=(boatOneCm/1000000)/playOneHour)) + 
+  barChart + legendTitle + coord_flip() +
+  xLable + labs(y="Distance (km) per Hour (real time)", title="Distance by Boat")
+ggsave(plot=p, file="Plots/statspage/boatOneCm_weighted.png", height=plotHeight, width=plotWidth)
+
+# Distance by Horse
+p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player,horseOneCm/playOneHour), y=(horseOneCm/1000000)/playOneHour)) + 
+  barChart + legendTitle + coord_flip() +
+  xLable + labs(y="Distance (km) per Hour (real time)", title="Distance by Horse")
+ggsave(plot=p, file="Plots/statspage/horseOneCm_weighted.png", height=plotHeight, width=plotWidth)
+
+# Distance by Minecart
+p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player,minecartOneCm/playOneHour), y=(minecartOneCm/1000000)/playOneHour)) + 
+  barChart + legendTitle + coord_flip() +
+  xLable + labs(y="Distance (km) per Hour (real time)", title="Distance by Minecart")
+ggsave(plot=p, file="Plots/statspage/minecartOneCm_weighted.png", height=plotHeight, width=plotWidth)
+
+# Distance by Pig
+p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player,pigOneCm/playOneHour), y=(pigOneCm/1000)/playOneHour)) + 
+  barChart + legendTitle + coord_flip() +
+  xLable + labs(y="Distance (m) per Hour (real time)", title="Distance by Pig")
+ggsave(plot=p, file="Plots/statspage/pigOneCm_weighted.png", height=plotHeight, width=plotWidth)
+
+# Distance Climbed
+p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player,climbOneCm/playOneHour), y=(climbOneCm/1000000)/playOneHour)) + 
+  barChart + legendTitle + coord_flip() +
+  xLable + labs(y="Distance (km) per Hour (real time)", title="Distance Climbed")
+ggsave(plot=p, file="Plots/statspage/climbOneCm_weighted.png", height=plotHeight, width=plotWidth)
+
+# Distance Dove
+p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player,diveOneCm/playOneHour), y=(diveOneCm/1000000)/playOneHour)) + 
+  barChart + legendTitle + coord_flip() +
+  xLable + labs(y="Distance (km) per Hour (real time)", title="Distance Dove")
+ggsave(plot=p, file="Plots/statspage/diveOneCm_weighted.png", height=plotHeight, width=plotWidth)
+
+# Distance Fallen
+p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player,fallOneCm/playOneHour), y=(fallOneCm/1000000)/playOneHour)) + 
+  barChart + legendTitle + coord_flip() +
+  xLable + labs(y="Distance (km) per Hour (real time)", title="Distance Fallen")
+ggsave(plot=p, file="Plots/statspage/fallOneCm_weighted.png", height=plotHeight, width=plotWidth)
+
+# Distance Flown
+p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player,flyOneCm/playOneHour), y=(flyOneCm/1000000)/playOneHour)) + 
+  barChart + legendTitle + coord_flip() +
+  xLable + labs(y="Distance (km) per Hour (real time)", title="Distance Flown")
+ggsave(plot=p, file="Plots/statspage/flyOneCm_weighted.png", height=plotHeight, width=plotWidth)
+
+# Distance Swum
+p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player,swimOneCm/playOneHour), y=(swimOneCm/1000000)/playOneHour)) + 
+  barChart + legendTitle + coord_flip() +
+  xLable + labs(y="Distance (km) per Hour (real time)", title="Distance Swum")
+ggsave(plot=p, file="Plots/statspage/swimOneCm_weighted.png", height=plotHeight, width=plotWidth)
+
+# Distance Walked
+p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player,walkOneCm/playOneHour), y=(walkOneCm/1000000)/playOneHour)) + 
+  barChart + legendTitle + coord_flip() +
+  xLable + labs(y="Distance (km) per Hour (real time)", title="Distance walked")
+ggsave(plot=p, file="Plots/statspage/walkOneCm_weighted.png", height=plotHeight, width=plotWidth)
+
+# Fish Caught
+p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player,fishCaught/playOneHour), y=fishCaught/playOneHour)) +
+  barChart + legendTitle + coord_flip() +
+  xLable + labs(y="Fish per Hour (real time)", title="Fish Caught")
+ggsave(plot=p, file="Plots/statspage/fishCaught_weighted.png", height=plotHeight, width=plotWidth)
 
 # Games quit
 p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player,leaveGame/playOneHour), y=leaveGame/playOneHour)) +
