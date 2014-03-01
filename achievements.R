c26b3fa43dce25880a7bef2894be50e5d62f679d
Jemus42 <jemus42@gmail.com>
2014-03-01 15:37:34 +0100

    Generate all achievements charts

--- achievements.R
+++ achievements.R
@@ -7,40 +7,37 @@ if((as.numeric(format(Sys.time(), "%s")) - as.numeric(now))/60/60 > 6){
 
 source("functions.R")
 
-# Cakes baked
-ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player,bakeCake), y=bakeCake)) + 
-  barChart + legendTitle + coord_flip() +
-  xLable + labs(y="Cakes", title="Cakes baked per player")
-ggsave(file="Plots/achievements/CakesBaked.png", height=plotHeight, width=plotWidth)
+## Generate basic plots for all achievements
+for(i in 1:nrow(achievementStrings)){
 
-# Cows killed
-ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player,killCow), y=killCow)) + 
-  barChart + legendTitle + coord_flip() +
-  xLable + labs(y="Cows", title="Cows killed per player")
-ggsave(file="Plots/achievements/CowsKilled.png", height=plotHeight, width=plotWidth)
+  if(achievementStrings$id[i] == "exploreAllBiomes"){ next };	
+
+  Filename <- paste("Plots/achievements/",achievementStrings$id[i],".png", sep="")
+  
+  p <- ggplot(data=playerstats)
+  p <- p + aes(fill=joinStatus, x=reorder(player, playerstats[,achievementStrings$id[i]]), 
+                                y=playerstats[,achievementStrings$id[i]]) 
+  p <- p + barChart + legendTitle + coord_flip() + scale_y_discrete(breaks= pretty_breaks())
+  p <- p + xLable + ylab("Times Achieved")
+  p <- p + ggtitle(paste("Achievement:",achievementStrings$displayname[i],"\n",achievementStrings$description[i]))
+  
+  ggsave(plot=p, file=Filename, height=plotHeight, width=plotWidth)
+
+}; rm(i); rm(Filename)
+
+
+## Generate selected weighted charts
 
 # Cow kill to cow breed ratio
 cowRatio <- as.numeric(sub("NaN", "0", playerstats$breedCow/(playerstats$killCow)))
 ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player,cowRatio), y=cowRatio)) + 
   barChart + legendTitle + coord_flip() +
   xLable + labs(y="Breeds per kills", title="Cow Breed to Cow Kill Ratio")
-ggsave(file="Plots/achievements/CowsRatio.png", height=plotHeight, width=plotWidth)
+ggsave(file="Plots/achievements/breedCow_by_killCow.png", height=plotHeight, width=plotWidth)
 rm(cowRatio);
-
-# Hoes made
-ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player,buildHoe), y=buildHoe)) + 
-  barChart + legendTitle + coord_flip() +
-  xLable + labs(y="Hoes", title="Hoes made per player")
-ggsave(file="Plots/achievements/HoesBuilt.png", height=plotHeight, width=plotWidth)
-
-# Withers killed
-ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player,killWither), y=killWither)) + 
-  barChart + legendTitle + coord_flip() +
-  xLable + labs(y="Withers", title="Withers killed per player")
-ggsave(file="Plots/achievements/WithersKilled.png", height=plotHeight, width=plotWidth)
 
 # Number of opened inventories per played hour
 ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player,openInventory/playOneHour), y=openInventory/playOneHour)) + 
   barChart + legendTitle + coord_flip() + scale_y_discrete(breaks= pretty_breaks()) +
   xLable + labs(y="Inventories Opened per Hour", title="How Often Players Opened Their Inventories per Hour")
-ggsave(file="Plots/achievements/OpenInventory_weighted.png", height=plotHeight, width=plotWidth)
+ggsave(file="Plots/achievements/openInventory_by_Time.png", height=plotHeight, width=plotWidth)
