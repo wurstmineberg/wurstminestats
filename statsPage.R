ce9359093582e09979fc217c8f761ec5977041c2
Jemus42 <jemus42@gmail.com>
2014-03-01 05:18:45 +0100

    Significantly compacted statsPage code

--- statsPage.R
+++ statsPage.R
@@ -8,164 +8,32 @@ if((as.numeric(format(Sys.time(), "%s")) - as.numeric(now))/60/60 > 6){
 
 source("functions.R")
 
-# # Get general statistics from playerstats # Experimental!
-# 
-# generalStats <- names(playerstats[names(strings$general)])
-# generalStatNames <- unlist(strings$general[2,], use.names=F)
-# 
-# # Generate general stats barcharts ## Experimental
-# for(i in 1:length(generalStats)){
-#   
-#   Filename <- paste("Plots/statspage/", generalStats[i],".png", sep="")
-#   
-#   p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player, playerstats[, generalStats[i]]), y=playerstats[, generalStats[i]])) + 
-#     barChart + legendTitle + coord_flip() + scale_y_discrete(breaks=pretty_breaks()) +
-#     xLable + labs(y="Units", title=generalStatNames[i])
-#   ggsave(plot=p, file=Filename, height=plotHeight, width=plotWidth)
-#   
-# }; rm(i); rm(Filename); rm(p);
+# Get general statistics from playerstats, define metadata (scale, units)
 
-############################
+statNum <- ncol(strings$general)
+generalStats <- data.frame(id=rep(0,statNum), name=rep(0,statNum), 
+                           units=rep(0,statNum), scale=rep(0,statNum))
+generalStats$id <- names(playerstats[names(strings$general)])
+generalStats$name <- unlist(strings$general[2,], use.names=F)
+generalStats$unit <- c("Animals", "km", "km", "Hearts (thousands)", "Hearts (thousands)", "Deathcount", "km", "Items", "km", "Fish", "km", "km", "Jumps (thousands)", "Junk", "Quits", "km", "Mobs", "m", "Hours (real life)", "Kills", "km", "Treasure", "km")
+generalStats$scale <- c(1,10^6,10^6,2000,2000,1,10^6,1,10^6,1,10^6,10^6,1000,1,1,10^6,1,1000,(20*60*60),1,10^6,1,10^6)
 
-# Animals Bred
-p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player, animalsBred), y=animalsBred)) + 
-  barChart + legendTitle + coord_flip() +
-  xLable + labs(y="Animals", title="Animals Bred")
-ggsave(plot=p, file="Plots/statspage/animalsBred.png", height=plotHeight, width=plotWidth)
+# Generate general stats barcharts ## Experimental
+for(i in 1:statNum){
+   
+   Filename <- paste("Plots/statspage/", generalStats$id[i],".png", sep="")
+   
+   p <- ggplot(data=playerstats) + 
+  aes(fill=joinStatus, x=reorder(player, playerstats[, generalStats$id[i]]), 
+         y=playerstats[, generalStats$id[i]]/generalStats$scale[i]) +
+   barChart + legendTitle + coord_flip() + scale_y_discrete(breaks=pretty_breaks()) +
+   xLable + labs(y=generalStats$unit[i], title=generalStats$name[i])
+   ggsave(plot=p, file=Filename, height=plotHeight, width=plotWidth)
+   
+}; rm(i, Filename, p, statNum)
 
-# Damage Dealt
-p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player, damageDealt), y=damageDealt/2000)) + 
-  barChart + legendTitle + coord_flip() +
-  xLable + labs(y="Hearts (thousands)", title="Damage Dealt")
-ggsave(plot=p, file="Plots/statspage/damageDealt.png", height=plotHeight, width=plotWidth)
 
-# Damage Taken
-p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player, damageTaken), y=damageTaken/2000)) + 
-  barChart + legendTitle + coord_flip() +
-  xLable + labs(y="Hearts (thousands)", title="Damage Taken")
-ggsave(plot=p, file="Plots/statspage/damageTaken.png", height=plotHeight, width=plotWidth)
-
-# Distance by Boat
-p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player, boatOneCm), y=(boatOneCm/1000000))) + 
-  barChart + legendTitle + coord_flip() +
-  xLable + labs(y="Distance (km)", title="Distance by Boat")
-ggsave(plot=p, file="Plots/statspage/boatOneCm.png", height=plotHeight, width=plotWidth)
-
-# Distance by Horse
-p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player, horseOneCm), y=(horseOneCm/1000000))) + 
-  barChart + legendTitle + coord_flip() +
-  xLable + labs(y="Distance (km)", title="Distance by Horse")
-ggsave(plot=p, file="Plots/statspage/horseOneCm.png", height=plotHeight, width=plotWidth)
-
-# Distance by Minecart
-p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player, minecartOneCm), y=(minecartOneCm/1000000))) + 
-  barChart + legendTitle + coord_flip() +
-  xLable + labs(y="Distance (km)", title="Distance by Minecart")
-ggsave(plot=p, file="Plots/statspage/minecartOneCm.png", height=plotHeight, width=plotWidth)
-
-# Distance by Pig
-p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player, pigOneCm), y=(pigOneCm/1000))) + 
-  barChart + legendTitle + coord_flip() +
-  xLable + labs(y="Distance (m)", title="Distance by Pig")
-ggsave(plot=p, file="Plots/statspage/pigOneCm.png", height=plotHeight, width=plotWidth)
-
-# Distance Climbed
-p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player, climbOneCm), y=(climbOneCm/1000000))) + 
-  barChart + legendTitle + coord_flip() +
-  xLable + labs(y="Distance (km)", title="Distance Climbed")
-ggsave(plot=p, file="Plots/statspage/climbOneCm.png", height=plotHeight, width=plotWidth)
-
-# Distance Dove
-p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player, diveOneCm), y=(diveOneCm/1000000))) + 
-  barChart + legendTitle + coord_flip() +
-  xLable + labs(y="Distance (km)", title="Distance Dove")
-ggsave(plot=p, file="Plots/statspage/diveOneCm.png", height=plotHeight, width=plotWidth)
-
-# Distance Fallen
-p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player, fallOneCm), y=(fallOneCm/1000000))) + 
-  barChart + legendTitle + coord_flip() +
-  xLable + labs(y="Distance (km)", title="Distance Fallen")
-ggsave(plot=p, file="Plots/statspage/fallOneCm.png", height=plotHeight, width=plotWidth)
-
-# Distance Flown
-p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player, flyOneCm), y=(flyOneCm/1000000))) + 
-  barChart + legendTitle + coord_flip() +
-  xLable + labs(y="Distance (km)", title="Distance Flown")
-ggsave(plot=p, file="Plots/statspage/flyOneCm.png", height=plotHeight, width=plotWidth)
-
-# Distance Swum
-p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player, swimOneCm), y=(swimOneCm/1000000))) + 
-  barChart + legendTitle + coord_flip() +
-  xLable + labs(y="Distance (km)", title="Distance Swum")
-ggsave(plot=p, file="Plots/statspage/swimOneCm.png", height=plotHeight, width=plotWidth)
-
-# Distance Walked
-p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player, walkOneCm), y=(walkOneCm/1000000))) + 
-  barChart + legendTitle + coord_flip() +
-  xLable + labs(y="Distance (km)", title="Distance walked")
-ggsave(plot=p, file="Plots/statspage/walkOneCm.png", height=plotHeight, width=plotWidth)
-
-# Fish Caught
-p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player, fishCaught), y=fishCaught)) +
-  barChart + legendTitle + coord_flip() +
-  xLable + labs(y="Amount of Fish", title="Fish Caught")
-ggsave(plot=p, file="Plots/statspage/fishCaught.png", height=plotHeight, width=plotWidth)
-
-# Games Quit
-p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player, leaveGame), y=leaveGame)) +
-  barChart + legendTitle + coord_flip() +
-  xLable + labs(y="Games Quit", title="Games Quit")
-ggsave(plot=p, file="Plots/statspage/leaveGame.png", height=plotHeight, width=plotWidth)
-
-# Items Dropped
-p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player, drop), y=drop)) + 
-  barChart + legendTitle + coord_flip() +
-  xLable+ labs(y="Items", title="Items Dropped")
-ggsave(plot=p, file="Plots/statspage/drop.png", height=plotHeight, width=plotWidth)
-
-# Jumps
-p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player, jump), y=jump/1000)) + 
-  barChart + legendTitle + coord_flip() +
-  xLable+ labs(y="Jumps (thousands)", title="Jumps")
-ggsave(plot=p, file="Plots/statspage/jump.png", height=plotHeight, width=plotWidth)
-
-# Junk Fished
-p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player, junkFished), y=junkFished)) +
-  barChart + legendTitle + coord_flip() +
-  xLable + labs(y="Amount of Junk", title="Junk Fished")
-ggsave(plot=p, file="Plots/statspage/junkFished.png", height=plotHeight, width=plotWidth)
-
-# Mob Kills
-p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player, mobKills), y=mobKills)) + 
-  barChart + legendTitle + coord_flip() +
-  xLable + labs(y="Mobs killed", title="Mob Kills")
-ggsave(plot=p, file="Plots/statspage/mobKills.png", height=plotHeight, width=plotWidth)
-
-# Number of Deaths
-p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player, deaths), y=deaths)) + 
-  barChart + legendTitle + coord_flip() +
-  xLable + labs(y="Deathcount", title="Number of Deaths")
-ggsave(plot=p, file="Plots/statspage/deaths.png", height=plotHeight, width=plotWidth)
-
-# Player Kills
-p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player, playerKills), y=playerKills)) + 
-  barChart + legendTitle + coord_flip() +
-  xLable + labs(y="Player Kills", title="Player Kills")
-ggsave(plot=p, file="Plots/statspage/playerKills.png", height=plotHeight, width=plotWidth)
-
-# Time Played / Idled
-p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player, playOneHour), y=playOneHour)) + 
-  barChart + legendTitle + coord_flip() +
-  xLable + labs(y="Online time (hours (real time))", title="Time Played / Idled") 
-ggsave(plot=p, file="Plots/statspage/playOneMinute.png", height=plotHeight, width=plotWidth)
-
-# Treasure Fished
-p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player, treasureFished), y=treasureFished)) +
-  barChart + legendTitle + coord_flip() +
-  xLable + labs(y="Amount of Treasure", title="Treasure Fished")
-ggsave(plot=p, file="Plots/statspage/treasureFished.png", height=plotHeight, width=plotWidth)
-
-###################### Weighted by hours played ###################### 
+###################### Weighted by hours played ######################
 
 # Animals Bred
 p <- ggplot(data=playerstats, aes(fill=joinStatus, x=reorder(player, animalsBred/playOneHour), y=animalsBred/playOneHour)) + 
