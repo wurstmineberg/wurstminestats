4cef7e1eaf9706521570cc1f139342cffd6f0c11
Jemus42 <jemus42@gmail.com>
2014-03-16 12:03:22 +0100

    Add chart for kills by mob with Endermen filtered out

--- entityStats.R
+++ entityStats.R
@@ -67,6 +67,15 @@ p <- p + barChart + coord_flip() + scale_y_discrete(breaks= pretty_breaks())
 p <- p + labs(x="Mobs", y="Kills", title="Killed Mobs")
 ggsave(plot=p, file="Plots/mobs/Kills_byMob.png", height=plotHeight, width=plotWidth)
 
+# … Let's filter out Endermen
+mobsKilled <- mobsKilled[mobsKilled$killedMob != "Enderman",]
+p <- ggplot(data=mobsKilled) 
+p <- p + aes(x=reorder(killedMob, nKills), y=nKills)
+p <- p + barChart + coord_flip() + scale_y_discrete(breaks= pretty_breaks())
+p <- p + labs(x="Mobs", y="Kills", title="Killed Mobs (except Endermen)")
+ggsave(plot=p, file="Plots/mobs/Kills_byMob_except_Endermen.png", height=plotHeight, width=plotWidth)
+
+# Deaths by mob
 mobsKilledBy <- data.frame(killedByMob=killedByEntityMobs,
                     nKilledBy=unlist(colwise(sum)(playerstats[killedByEntity]), use.names=F))
 
