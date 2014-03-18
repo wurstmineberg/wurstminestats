5ce2e9f700ddba4f0b681b5a674534ccf62a292e
Jemus42 <jemus42@gmail.com>
2014-03-18 10:30:58 +0100

    Get item num id vs new id shit sorted (P.S: dafuq)

--- itemStats.R
+++ itemStats.R
@@ -12,12 +12,7 @@ source("functions.R")
 
 
 # Exclude old item IDs because I really don't want to deal with that
-items <- items[grep("[^0-9]$", names(items))]
-
-# Sorting stuff
-craftItem <- items[grep("craftItem",names(items))]
-mineBlock <- items[grep("mineBlock",names(items))]
-useItem <- items[grep("useItem",names(items))]
+#items <- items[grep("[^0-9]$", names(items))]
 
 # Get items.json from our website for names/ids
 itemData <- fromJSON("http://wurstmineberg.de/static/json/items.json")
@@ -28,3 +23,33 @@ itemData$ID <- sub(":",".",itemData$ID)
 itemData$name <- unlist(itemData$name, use.names=F)
 # Get rid of everything else by subsetting
 itemData <- subset(itemData, select=c("numID","ID","name"))
+itemActions <- c("mineBlock.", "craftItem.", "useItem.", "breakItem.")
+
+# Get list of IDs actually existing in items dataset
+existingNumIDs <- names(items)[grep("\\.[0-9]+$", names(items))]
+existingNumIDs <- sub("craftItem.","", existingNumIDs)
+existingNumIDs <- sub("useItem.","", existingNumIDs)
+existingNumIDs <- sub("mineBlock.","", existingNumIDs)
+existingNumIDs <- sub("breakItem.","", existingNumIDs)
+existingNumIDs <- unique(existingNumIDs)
+existingIDs <- rep(NULL,length(existingNumIDs))
+for(i in 1:length(existingNumIDs)){
+  existingIDs[i] <- sub(existingNumIDs[i], itemData$ID[itemData$numID == existingNumIDs[i]], existingNumIDs[i])
+}; rm(i)
+
+# I honestly have no idea anymore what I did here
+for(i in 1:length(existingNumIDs)){
+  if(length(grep("TRUE", existingNumIDs[i] == itemData$numID)) == 1){
+    for(k in itemActions){
+      if(ncol(items[names(items) == paste(k,itemData$numID[i], sep="")]) == 1 & ncol(items[names(items) == paste(k,itemData$ID[i], sep="")]) == 1){
+      
+      itemOldID <- items[names(items) == paste(k,itemData$numID[i], sep="")]
+      itemNewID <- items[names(items) == paste(k,itemData$ID[i], sep="")]
+      itemNewID <- itemNewID + itemOldID 
+      }
+    }
+  }
+}; rm(i, k, itemOldID, itemNewID, existingNumIDs)
+
+# Exclude now unneeded old item ID columns
+items <- items[ , !names(items) %in% names(items)[grep("\\.[0-9]+$", names(items))]]
