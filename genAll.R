486338e4b3a83474bdd4f1724c711a0aa12f5b20
Jemus42 <jemus42@gmail.com>
2014-03-21 01:07:33 +0100

    Formatting/Cleanup/Comments

--- genAll.R
+++ genAll.R
@@ -1,9 +1,11 @@
 #! /usr/bin/Rscript
 ## Headless / IDE-less executable script to autogenerate all the plots
 
+# These are biggies and are required for everything else to work
 source("dataPrep.R")
 source("functions.R")
 
+# These are not
 source("Playerstats.R")
 source("achievements.R")
 source("statsPage.R")
