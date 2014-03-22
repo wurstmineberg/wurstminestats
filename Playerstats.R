86983477025ddb6cef1dc60e03ca212271704e9f
Jemus42 <jemus42@gmail.com>
2014-03-22 17:36:36 +0100

    Remove some useless graphs

--- Playerstats.R
+++ Playerstats.R
@@ -17,39 +17,6 @@ p <- p + ggtitle("Online Time by Server Age") + coord_flip()
 p <- p + scale_fill_manual(name = "Join Status", values = statusColours)
 ggsave(p, file="Plots/OnlineTimebyServerAge.png", height=plotHeight, width=plotWidth)
 
-# Deaths per online hour 
-p <- ggplot(playerstats, aes(x = playOneHour, y = deaths)) 
-p <- p + geom_point(aes(colour=joinStatus), group=1) 
-p <- p + labs(y="Deaths", x="Online time (hours (real time))") 
-p <- p + ggtitle("Deaths vs. Online time") 
-p <- p + playerTheme 
-p <- p + scale_colour_manual(name = "Join Status", values = statusColours) 
-ggsave(p, file="Plots/Deaths_OnlineTime.png", height=6, width=8)
-
-# Damage taken vs deaths
-p <- ggplot(playerstats, aes(y=deaths, x=(damageTaken/20000), label=player)) 
-p <- p + geom_point(aes(colour=joinStatus, group=1)) 
-p <- p + geom_text(size=2, hjust=-.2, vjust=.4) 
-p <- p + labs(x="Damage Taken (Hearts in thousands)", y="Deaths") 
-p <- p + ggtitle("Damage Taken vs. Deaths") 
-p <- p + playerTheme 
-p <- p + scale_colour_manual(name = "Join Status", values = statusColours) 
-ggsave(p, file="Plots/Deaths_DamageTaken.png", height=6, width=8)
-
-# Throw linear modeling at stuff
-summary(lm(playerstats$deaths ~ playerstats$damageTaken))
-cor(playerstats$deaths, playerstats$damageTaken)^2
-
-# Mob kills vs play time
-p <- ggplot(playerstats, aes(x=playOneHour, y=mobKills, label=player)) 
-p <- p + geom_point(aes(colour=joinStatus, group=1)) 
-p <- p + geom_text(size=2, hjust=-.2, vjust=.4) 
-p <- p + labs(y="Mob kills (absolute)", x="Online time (hours (real time)))") 
-p <- p + ggtitle("Mob kills vs. Online time") 
-p <- p + playerTheme 
-p <- p + scale_colour_manual(name = "Join Status", values = statusColours) 
-ggsave(p, file="Plots/MobKills_OnlineTime.png", height=plotHeight, width=plotWidth)
-
 # Distance walked per online time
 p <- ggplot(playerstats, aes(x = playOneHour,  y = (walkOneCm/1000000), label=player)) 
 p <- p + geom_point(aes(colour=joinStatus, group=1)) 
@@ -73,16 +40,6 @@ p <- p + scale_x_datetime(labels = date_format("%y-%m-%d"),
                           expand=c(.2,1));
 ggsave(p, file="Plots/WhitelistGrowth.png", height=6, width=8)
 
-# Play time vs server age
-p <- ggplot(playerstats, aes(x = serverAge, y = playOneHour, label=player)) 
-p <- p + geom_point(aes(colour=joinStatus, group=1)) 
-p <- p + geom_text(size=2, hjust=.5, vjust=-1) 
-p <- p + ylab("Time spent on server (hours)") + xlab("Age on server (days)") 
-p <- p + ggtitle("Time spent on server vs. relative age on server") 
-p <- p + playerTheme 
-p <- p + scale_colour_manual(name = "Join Status", values = statusColours) 
-ggsave(p, file="Plots/ServerAge_PlayTime.png", height=plotHeight, width=plotWidth)
-
 # Distance Traveled Total
 p <- ggplot(data=playerstats)
 p <- p + aes(fill=joinStatus, x=reorder(player,distanceTraveled), y=(distanceTraveled/1000000))
@@ -97,7 +54,9 @@ p <- p + barChart + legendTitle + coord_flip()
 p <- p + xLable + labs(y="Distance (km) per Hour (real time)", title="Total Distance Traveled by Online Time")
 ggsave(p, file="Plots/DistanceTraveled_weighted.png", height=plotHeight, width=plotWidth)
 
-### Death stats
+##################
+### Death stats ##
+##################
 
 deaths <- data.frame(player = names(latestdeaths$deaths[,1]))
 deaths$timestamp <- unlist(latestdeaths$deaths[,1], use.names=F)
@@ -110,6 +69,4 @@ p <- p + aes(x=reorder(player,daysSince), y=daysSince)
 p <- p + barChart + legendTitle + coord_flip()
 p <- p + xLable + labs(y="Days Since Death", title="Days Since Players' Latest Death")
 ggsave(p, file="Plots/LatestDeaths.png", height=plotHeight, width=plotWidth)
-
-
 rm(p)
