ddc5c3f39e705c6fe6ff1e250e4fcd0aa3916369
Jemus42 <jemus42@gmail.com>
2014-03-23 16:00:11 +0100

    Further purrdification

--- dataPrep.R
+++ dataPrep.R
@@ -136,3 +136,21 @@ items$player <- playerstats$player
 playerstats$timestamp <- now
 
 writePlayerstatsLog()
+
+# Define general legend/guide for all players
+playerTheme <- theme(legend.position="right",
+                    legend.key.size = unit(.4, "cm"),
+                    legend.text = element_text(size = rel(.8)));
+
+# Define some variables for bar chart layout and labels
+plotWidth <- 6; plotHeight <- 4;
+barChart <- geom_bar(colour="black", width=.7, stat="identity")
+xLable <- xlab("Player")
+
+
+# Define colour scale to keep status colours static
+if("playerstats" %in% ls()){
+    statusColours <- brewer.pal(9,"Set1")
+    names(statusColours) <- levels(playerstats$joinStatus)
+    legendTitle <- scale_fill_manual(name = "Join Status", values = statusColours)
+}
