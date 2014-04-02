e3ed6c3695c6fae6b4b069d67d3554175f7d9c32
jemus42 <jemus42@quantenbrot.de>
2014-04-02 14:39:44 +0200

    Add navbar layout basics / How do you people even live with all these (s 'n )s?

--- shiny/ui.R
+++ shiny/ui.R
@@ -1,36 +1,44 @@
 library(shiny)
 columns <- names(activePeople)
-shinyUI(fluidPage(
-  titlePanel("Wurstminedata", "Where data goes to rot"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      helpText("This is a demo displaying server members"),
-      br(),
-      helpText("You can choose the whitelisting period with the sliders below."),
-    
-      dateRangeInput("dates", 
-        h3("Date range"),
-        start = "2012-10-30", 
-        end = as.character(Sys.Date())),
-      
-      checkboxInput("onlycol", label = "Show only those with fav color set", value = FALSE),
-      
-      checkboxGroupInput("columnSelect", 
-                         label = h3("Select Colums"), 
-                         choices = columns,
-                         selected = c("name", "joinDate", "joinStatus", "serverAge", "color", "invitedBy"))
-   
-    #  actionButton("get", "Nothing yet")
-      
-    ),
-    
-    mainPanel(
-      h3(textOutput("birthdays")), br(),
-      tabsetPanel(
-        tabPanel("People", dataTableOutput("table1"))
-      )
-      
+shinyUI(navbarPage("Wurstminedata",
+  tabPanel("Data",
+    fluidPage(
+      sidebarLayout(
+        sidebarPanel(
+          helpText("This is a demo displaying server members"),
+          br(),
+          helpText("You can choose the whitelisting period with the sliders below."),
+        
+          dateRangeInput("dates", 
+            h3("Date range"),
+            start = "2012-10-30", 
+            end = as.character(Sys.Date())),
+          
+          checkboxInput("onlycol", label = "Show only those with fav color set", value = FALSE),
+          
+          checkboxGroupInput("columnSelect", 
+                             label = h3("Select Colums"), 
+                             choices = columns,
+                             selected = c("name", "joinDate", "joinStatus", "serverAge", "color", "invitedBy"))
+       
+        #  actionButton("get", "Nothing yet")
+          
+        ), # closes sidebarPanel
+        
+        mainPanel(
+          h3(textOutput("birthdays")), br(),
+          tabsetPanel(
+            tabPanel("People", dataTableOutput("table1"))
+          )
+        )
+      ) # Closes sidebarLayout
+    ) # Closes fluidPage
+  ),
+
+    tabPanel("About", 
+      fluidPage(
+          p("I should buy a goat")
+        )
     )
-  )
+
 ))
