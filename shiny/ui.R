ee0450734f292a64805b94cc4bcb400f36556f90
jemus42 <jemus42@quantenbrot.de>
2014-04-19 18:04:04 +0200

    Shiny: Update controls to selectize, add more brokeness

--- shiny/ui.R
+++ shiny/ui.R
@@ -1,6 +1,6 @@
 library(shiny)
 require(rCharts)
-options(RCHART_LIB = 'polycharts')
+#options(RCHART_LIB = 'polycharts')
 
 columns        <- names(activePeople)
 columnsPreset  <- c("name", "joinDate", "joinStatus", "color", "invitedBy")
@@ -9,15 +9,15 @@ sessionsPeople <- activePeople$name[activePeople$name %in% playedPerPerson$perso
 # current server age total
 wurstminebergAge <- round(as.numeric(difftime(Sys.time(), activePeople$joinDate[1], units ="days")))
 
-shinyUI(navbarPage(title="Wurstminedata", id="nav", inverse=TRUE,
-  tabPanel(title="Data", value="data",
+shinyUI(navbarPage(title = "Wurstminedata", id = "nav", inverse = TRUE,
+  tabPanel(title = "Data", value = "data",
       pageWithSidebar(
 
         headerPanel("Warning: Early dev shit"),
 
         sidebarPanel(
           conditionalPanel(
-            condition="input.dataTab == 'dataPeople'",
+            condition = "input.dataTab == 'dataPeople' ",
             h3("People Filters"),
             helpText("You can choose the whitelisting period with the sliders below."),
           
@@ -28,18 +28,30 @@ shinyUI(navbarPage(title="Wurstminedata", id="nav", inverse=TRUE,
             
             checkboxInput("onlycol", label = "Show only those with fav color set", value = FALSE),
             
-            checkboxGroupInput("columnSelect", 
+            selectInput("columnSelect", 
                                label = h4("Colums to Display"), 
                                choices = columns,
-                               selected = columnsPreset)
+                               selected = columnsPreset,
+                               multiple= TRUE,
+                               selectize = TRUE)
             ),
           conditionalPanel(
-            condition="input.dataTab == 'dataSessions'",
+            condition = "input.dataTab == 'dataSessions' ",
             h3("Session Filters"),
               dateRangeInput("sessionDates", h4("Date range"),
                 start = "2014-02-19", 
                 end = as.character(Sys.Date())
               )
+          ),
+          conditionalPanel(
+            condition = "input.dataTab == 'dataDeaths' ",
+            h3("Latest deaths"),
+            helpText("These are the latest deaths only, since there currently is no API endpoint for the complete death log")
+          ),
+          conditionalPanel(
+            condition = "input.dataTab == 'dataItemStats' ",
+            h3("Item stats per item/action"),
+            helpText("You can filter the dataset with the search boxes on the right, besides that I don't really know what to offer you.")
           )
         ), # closes sidebarPanel
         
@@ -67,20 +79,37 @@ shinyUI(navbarPage(title="Wurstminedata", id="nav", inverse=TRUE,
       headerPanel("These are plots. They are mostly broken as of now, sorry."),
       sidebarPanel(
         h3("Plot controls"),
-        dateRangeInput("datesPlot", h4("Select Time Period"),
-             start = as.character(Sys.Date()-14), 
-             end   = as.character(Sys.Date())
+          conditionalPanel(
+            condition = "input.plotTab == 'plotSessions' ",
+
+              dateRangeInput("datesPlot", h4("Select Time Period"),
+                            start = as.character(Sys.Date()-14), 
+                            end   = as.character(Sys.Date())
+              ),
+              selectInput("line.or.bar", "Chart type", choices =c("Line", "Bar"), selected = "Bar"),
+              selectInput("date.scope", "Date scope", choices =c("Daily", "Weekdays"), selected = "Daily")
+          ),
+          conditionalPanel(
+            condition = "input.plotTab == 'plotGeneralStats' ",
+              selectInput("selectGeneralStat",
+                          label = h4("Select a statistic"),
+                          choices = generalStats$name,
+                          selected = generalStats$name[1],
+                          multiple = FALSE,
+                          selectize= TRUE)
+          ),
+          selectInput("columnSelectPlot", 
+                             label    = h4("People to Display"), 
+                             choices  = sessionsPeople,
+                             selected = sessionsPeople,
+                             multiple = TRUE,
+                             selectize = TRUE)
         ),
-        selectInput("line.or.bar", "Chart type", choices =c("Line", "Bar"), selected = "Bar"),
-        selectInput("date.scope", "Date scope", choices =c("Daily", "Weekdays"), selected = "Daily"),
-        checkboxGroupInput("columnSelectPlot", 
-                           label    = h4("People to Display"), 
-                           choices  = sessionsPeople,
-                           selected = sessionsPeople)
-        ),
-      mainPanel(tabsetPanel(
-        tabPanel("Played Time – ggplot2", plotOutput("sessionPlot")),
-        tabPanel("Played Time – rCharts (NYI)", showOutput("sessionPlot2", "polycharts"))
+      mainPanel(tabsetPanel(id = "plotTab",
+        tabPanel("Played Time – ggplot2",       value = "plotSessions",     plotOutput("sessionPlot")),
+        tabPanel("Played Time – rCharts (NYI)", value = "pltoSessions",     showOutput("sessionPlot2", "polycharts")),
+        tabPanel("Played Time – gvis (WIP)",    value = "plotSessions",     htmlOutput("plot.session.gvis")),
+        tabPanel("General Stats",               value = "plotGeneralStats", showOutput("plot.generalStats", "NVD3"))
       ))
     )
   ),
