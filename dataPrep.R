0eb4b925e763c7d76ca591da0473076d72445c8a
jemus42 <jemus42@quantenbrot.de>
2014-04-23 12:35:57 +0200

    Formatting

--- dataPrep.R
+++ dataPrep.R
@@ -1,4 +1,4 @@
-## Datapreparations
+#### Datapreparations ####
 
 # Startup
 if(grepl("shiny$", getwd())){
@@ -12,21 +12,21 @@ if(grepl("shiny$", getwd())){
 # Get a close enough timestamp for the data age. Reimport via as.POSIXct(x,origin="1970-01-01") should be sufficient, else handle as.numeric()
 dataTime <- format(Sys.time(), "%s")
 
-## Get player stats from wurstmineberg API ##
-generalstats  <- fromJSON(getOption("url.stats.general"))
-achievements  <- fromJSON(getOption("url.stats.achievements"))
-entities      <- fromJSON(getOption("url.stats.entities"))
-items         <- fromJSON(getOption("url.stats.items"))
+#### Get player stats from wurstmineberg API ####
+generalstats            <- fromJSON(getOption("url.stats.general"))
+achievements            <- fromJSON(getOption("url.stats.achievements"))
+entities                <- fromJSON(getOption("url.stats.entities"))
+items                   <- fromJSON(getOption("url.stats.items"))
 
-# Get strings.json for some… strings. (Mob IDs, display names)
+#### Get strings.json for some… strings. (Mob IDs, display names) ####
 strings.general         <- getStrings(category = "general")
 strings.mobs            <- getStrings(category = "mobs")
 strings.achievements    <- getStrings(category = "achievements")
 strings.items           <- getStrings(category = "items")
 
-####################################################################################
+#----------------------------------------------------------------------------------#
 #### This is where imported datasets get cleaned up so we can actually use them ####
-####################################################################################
+#----------------------------------------------------------------------------------#
 
 ## Getting a people dataset from people.json ## (Also, deaths)
 activePeople  <- getActivePeople()
@@ -39,77 +39,70 @@ achievements  <- stats2df(achievements, type = "achievements")
 entities      <- stats2df(entities)
 items         <- stats2df(items)
 
-#################################################
-## Enhancing playerstats with some useful shit ##
-#################################################
+#---------------------------------------------------#
+#### Enhancing playerstats with some useful shit ####
+#---------------------------------------------------#
 
 ## Convert play time to real time hours as separate column ##
 generalstats$playOneHour <- (generalstats$playOneMinute/20/60/60)
-
 ## Get total distance column by summing up all *OneCm rows per player ##
 generalstats$distanceTraveled <- rowSums(generalstats[, grep("OneCm", colnames(generalstats))])
-
 ## Resort columns to get interesting stuff first. ##
-
-playerstats   <- join(generalstats, achievements)
-playerstats   <- join(playerstats, entities)
-
+playerstats             <- join(generalstats, achievements)
+playerstats             <- join(playerstats, entities)
+## Append other useful meta info 
 playerstats$joinStatus  <- activePeople$joinStatus
 playerstats$joinDate    <- activePeople$joinDate
-
-playerstats$timestamp <- dataTime
-
+playerstats$timestamp   <- dataTime
+## Reordering columns
 generalColumns <- c("timestamp", "player", "joinDate", "joinStatus", "leaveGame",
                     "deaths", "timeSinceDeath", "playerKills", "damageDealt", "damageTaken", "playOneMinute", 
                     "playOneHour", "jump", "animalsBred", "mobKills")
-
 playerstats <- playerstats[c(generalColumns, setdiff(names(playerstats), generalColumns))]
 rm(generalColumns)
 
-# One last time to make sure
+## One last time to make sure
 playerstats[is.na(playerstats)] <- 0
 
-##########################
-## Handle items dataset ##
-##########################
+#----------------------------#
+#### Handle items dataset ####
+#----------------------------#
 
 ## Get table of item actions and readable names ##
 itemActions     <- data.frame(id    = c("mineBlock", "craftItem"   , "useItem" , "breakItem"), 
                               name  = c("mined"    , "crafted"     , "used"    , "broken"))
 
 ## Merge old and new item stat IDs and whate have you ##
-items     <- mergeItemStats(items, itemActions, strings.items)
+items           <- mergeItemStats(items, itemActions, strings.items)
 
 ## Get a dataframe of item stat ID, item name and action ##
-itemStats <- getItemStats(items, itemActions, strings.items)
+itemStats       <- getItemStats(items, itemActions, strings.items)
 
 ## Finally join the remaining datasets ## 
-playerstats <- join(playerstats, items, type="full")
+playerstats     <- join(playerstats, items, type="full")
 
-###################################################
-## Getting sessions from /sessions/overview.json ##
-###################################################
+#-----------------------------------------------------#
+#### Getting sessions from /sessions/overview.json ####
+#-----------------------------------------------------#
 
 sessions        <- getSessions()
 playerSessions  <- getPlayerSessions(sessions)
-
-# Ideally sessions should be separated per day, I guess?
+## Ideally sessions should be separated per day, I guess?
 playerSessions  <- splitSessionsByDay(playerSessions)
 
-# We want play time per day, sooooo… ##
+## We want play time per day, sooooo… 
 playedPerDay  <- ddply(playerSessions, .(date, wday), summarize, timePlayed = sum(playedMinutes))
-
 # We also want play time per day per person, so, well… ##
 playedPerPerson <- getPlayedPerPerson(playerSessions)
-
 # Getting per weekday stuff
 playedPerWeekday      <- playedPerPerson
 playedPerWeekday      <- ddply(playedPerPerson, .(wday, person), summarize, timePlayed=sum(timePlayed))
 avgPerWeekday         <- mean(ddply(playedPerWeekday, .(wday), summarize, timePlayed=sum(timePlayed))$timePlayed)
-
 # Let's do a monthly one
 playedPerMonth       <- ddply(playedPerPerson, .(month, person), summarize, timePlayed=sum(timePlayed))
 avgPerMonth          <- mean(ddply(playedPerMonth, .(month), summarize, timePlayed=sum(timePlayed))$timePlayed)
+# Actually per person
+playtime.people <- ddply(playedPerPerson, "person", summarize, timePlayed=sum(timePlayed))
 
 # Fix playerSession person names
 for(i in playerSessions$person){
--- drawPlots.R
+++ drawPlots.R
@@ -87,7 +87,6 @@ p <- p + geom_hline(yintercept = avgPerMonth/60, alpha=.5)
 p <- p + labs(y="Played Hours", x="Months", title="Total Time Played per Month")
 p <- p + scale_fill_manual(name="People", values=fillColours) + playerTheme
 ggsave(p, file="Plots/sessions/playTime_months.png", height=6, width=12)
-rm(p, fillColours)
 
 # JoinTime hours histogram
 fillColours   <- activePeople$color[activePeople$name %in% playerSessions$person]
