9ca731265943c516767b61e91f544a7739fccb89
jemus42 <jemus42@quantenbrot.de>
2014-05-01 21:49:13 +0000

    Let's stick with inverse stacking order for consistency's sake

--- drawPlots.R
+++ drawPlots.R
@@ -65,7 +65,7 @@ ggsave(p, file="Plots/sessions/playTime.png", height=6, width=12)
 # Testing "played per weekday"
 fillColours   <- activePeople$color[activePeople$name %in% playedPerWeekday$person]
 
-p <- ggplot(data=arrange(playedPerWeekday, desc(person)))
+p <- ggplot(data=arrange(playedPerWeekday, person))
 p <- p + aes(x=wday, y=timePlayed/60, fill=person)
 p <- p + geom_bar(position="stack", stat="identity", colour="black")
 p <- p + geom_hline(yintercept = avgPerWeekday/60, alpha=.5)
@@ -77,7 +77,7 @@ rm(avgPerWeekday)
 # Plotting playedPerPerson 
 fillColours <- activePeople$color[activePeople$name %in% playedPerPerson$person]
 
-p <- ggplot(data=arrange(playedPerPerson, desc(person)), aes(x=format(date, "%d"), y=timePlayed/60, fill=person))
+p <- ggplot(data=arrange(playedPerPerson, person), aes(x=format(date, "%d"), y=timePlayed/60, fill=person))
 p <- p + geom_bar(position="stack", stat="identity", colour="black")
 p <- p + theme(axis.text.x = element_text(angle = 45, hjust = 1)) 
 #p <- p + scale_x_datetime(labels = date_format("%y-%m-%d"), breaks = date_breaks("days"))
@@ -92,7 +92,7 @@ rm(p, fillColours, monthNum)
 # Plotting playedPerMonth 
 fillColours   <- activePeople$color[activePeople$name %in% playedPerMonth$person]
 
-p <- ggplot(data=arrange(playedPerMonth, desc(person)))
+p <- ggplot(data=arrange(playedPerMonth, person))
 p <- p + aes(x=month, y=timePlayed/60, fill=person)
 p <- p + geom_bar(position="stack", stat="identity", colour="black")
 p <- p + geom_hline(yintercept = avgPerMonth/60, alpha=.5)
@@ -103,7 +103,7 @@ rm(p)
 # JoinTime hours histogram
 fillColours   <- activePeople$color[activePeople$name %in% playerSessions$person]
 
-p <- ggplot(data=arrange(playerSessions, desc(person)))
+p <- ggplot(data=arrange(playerSessions, person))
 p <- p + aes(x=hour(playerSessions$joinTime), fill=person)
 p <- p + geom_histogram(colour="black", binwidth=.7)
 p <- p + labs(y="Frequency", x="Hour of Day (UTC)", title="Join Time Frequencies")
@@ -113,7 +113,7 @@ p <- p + scale_y_continuous(breaks=pretty_breaks())
 ggsave(p, file="Plots/sessions/joinTime_hours.png", height=6, width=12)
 rm(p)
 # LeaveTime hours histogram because it's only right
-p <- ggplot(data=arrange(playerSessions, desc(person)))
+p <- ggplot(data=arrange(playerSessions, person))
 p <- p + aes(x=hour(playerSessions$leaveTime), fill=person)
 p <- p + geom_histogram(colour="black", binwidth=.7)
 p <- p + labs(y="Frequency", x="Hour of Day (UTC)", title="Leave Time Frequencies")
