a8dc3cf3114ee15350ecc9895d009dad10fb36a9
jemus42 <jemus42@quantenbrot.de>
2014-05-01 20:25:40 +0000

    Fix bar stack ordering, only do item stat plotting on sunday evenings

--- drawPlots.R
+++ drawPlots.R
@@ -77,11 +77,10 @@ rm(avgPerWeekday)
 # Plotting playedPerPerson 
 fillColours <- activePeople$color[activePeople$name %in% playedPerPerson$person]
 
-p <- ggplot(data=playedPerPerson, aes(x=format(date, "%d"), y=timePlayed/60, fill=person))
+p <- ggplot(data=arrange(playedPerPerson, desc(person)), aes(x=format(date, "%d"), y=timePlayed/60, fill=person))
 p <- p + geom_bar(position="stack", stat="identity", colour="black")
 p <- p + theme(axis.text.x = element_text(angle = 45, hjust = 1)) 
-#p <- p + scale_x_datetime(labels = date_format("%y-%m-%d"),
-#                          breaks = date_breaks("days"))
+#p <- p + scale_x_datetime(labels = date_format("%y-%m-%d"), breaks = date_breaks("days"))
 p <- p + scale_y_continuous(breaks=pretty_breaks()) + playerTheme
 p <- p + labs(y="Played Hours", x="Day of Month (UTC)", title="Time Played per Day: 2014")
 p <- p + scale_fill_manual(name="People", values=fillColours)
@@ -237,46 +236,47 @@ ggsave(p, file="Plots/achievements/exploreAllBiomesProgress.png", height=plotHei
 #----------------------------------------#
 #### Plotting item stats as they come ####
 #----------------------------------------#
-cat("Generating item stats plots \n")
-
-for(i in 1:length(itemStats$stat)){
-
-    stat        <- itemStats$stat[i]
-    action      <- itemStats$action[i]
-    itemName    <- itemStats$item[i]
-    filename    <- paste("Plots/items/", action, "/", stat, ".png", sep="")
-    title       <- paste("Times item was ", action, ": ", itemName, sep="")
-
-    p <- ggplot(data=playerstats)
-    p <- p + aes(fill=joinStatus, x=reorder(player, playerstats[, stat], mean, order=T), y=playerstats[, stat])
-    p <- p + barChart + statusFillScale + coord_flip() + scale_y_discrete(breaks= pretty_breaks())
-    p <- p + labs(x="Player", y=paste("Times", action), title=title)
+if (wday(now(), T, F) == "Sunday" && hour(now()) > 20){}
+  cat("Generating item stats plots \n")
+  
+  for(i in 1:length(itemStats$stat)){
+  
+      stat        <- itemStats$stat[i]
+      action      <- itemStats$action[i]
+      itemName    <- itemStats$item[i]
+      filename    <- paste("Plots/items/", action, "/", stat, ".png", sep="")
+      title       <- paste("Times item was ", action, ": ", itemName, sep="")
+  
+      p <- ggplot(data=playerstats)
+      p <- p + aes(fill=joinStatus, x=reorder(player, playerstats[, stat], mean, order=T), y=playerstats[, stat])
+      p <- p + barChart + statusFillScale + coord_flip() + scale_y_discrete(breaks= pretty_breaks())
+      p <- p + labs(x="Player", y=paste("Times", action), title=title)
+      
+      cat(paste("Saving", filename, "\n"))
+      ggsave(plot=p, file=filename, height=plotHeight, width=plotWidth)
+  
+  }; rm(p, i, title, filename, stat, action, itemName)
+  
+  ## Now to look at the different item actions ##
+  cat("Generating item actions plots \n")
+  
+  # subset for each action, get top 20 items for each action, and plot them
+  for(action in itemActions$name){
+    itemStatsPerAction <- itemStats[itemStats$action == action,]
+    itemStatsPerAction <- head(arrange(itemStatsPerAction, desc(total)), 20)
+    filename           <- paste("Plots/items/top_", action, ".png", sep="")
+    
+    p <- ggplot(data=itemStatsPerAction)
+    p <- p + aes(x=sortLevels(item, total), y=total/1000)
+    p <- p + barChart + coord_flip()
+    p <- p + labs(x="Item", y=paste("Times", action, "(in thousands)", sep=" "))
+    p <- p + ggtitle(paste("Top", action, "items", sep=" "))
     
     cat(paste("Saving", filename, "\n"))
     ggsave(plot=p, file=filename, height=plotHeight, width=plotWidth)
-
-}; rm(p, i, title, filename, stat, action, itemName)
-
-## Now to look at the different item actions ##
-cat("Generating item actions plots \n")
-
-# subset for each action, get top 20 items for each action, and plot them
-for(action in itemActions$name){
-  itemStatsPerAction <- itemStats[itemStats$action == action,]
-  itemStatsPerAction <- head(arrange(itemStatsPerAction, desc(total)), 20)
-  filename           <- paste("Plots/items/top_", action, ".png", sep="")
-  
-  p <- ggplot(data=itemStatsPerAction)
-  p <- p + aes(x=sortLevels(item, total), y=total/1000)
-  p <- p + barChart + coord_flip()
-  p <- p + labs(x="Item", y=paste("Times", action, "(in thousands)", sep=" "))
-  p <- p + ggtitle(paste("Top", action, "items", sep=" "))
-  
-  cat(paste("Saving", filename, "\n"))
-  ggsave(plot=p, file=filename, height=plotHeight, width=plotWidth)
-  
-}; rm(action, itemStatsPerAction)
-
+    
+  }; rm(action, itemStatsPerAction)
+}
 #------------------------------------------------------#
 #### Dealing with entitiy stats. Kind of a big one. ####
 #------------------------------------------------------#
