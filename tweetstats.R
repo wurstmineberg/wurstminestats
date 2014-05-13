c624aab8ce5a609eb9b07b973924ee97f1e6aed2
jemus42 <jemus42@quantenbrot.de>
2014-05-13 23:38:00 +0200

    Only one statsweet at a time now

--- tweetstats.R
+++ tweetstats.R
@@ -6,30 +6,31 @@ source("options.R")
 source("functions.R")
 
 # Make twitteR work
-if(!exists("twitCred")){
-  load("cache/twitcred.RData")
-  print(ifelse(registerTwitterOAuth(twitCred), "Tweets should work", "Something broke"))
+load("cache/twitcred.RData")
+registerTwitterOAuth(twitCred)
+
+# Get random stats out, only one now.
+diceroll <- round(runif(1,1,4))
+if (diceroll == 1){
+  tweet(statOfTheDay(category = sample(c("general", "items", "mobs"), 1)))
+} else if (diceroll == 2){
+  tweet(randomItemStat())
+} else if (diceroll == 3){
+  tweet(randomMobStat())
+} else if (diceroll == 4){
+  tweet(randomAchievement("random"))
 }
 
-# Get random stats out
-tweet(statOfTheDay(category = sample(c("general", "items", "mobs"), 1)))
-Sys.sleep(60)
-tweet(randomItemStat())
-Sys.sleep(60)
-tweet(randomMobStat())
-Sys.sleep(60)
-tweet(randomAchievement("random"))
-
 # Special tweets
-if (wday(now(), T, F) == "Sunday" && hour(now()) %in% 20:24){
+if (wday(now(), T, F) == "Sunday" && hour(now()) == 22){
   Sys.sleep(30)
   tweet(mostActiveDay(7))
 }
-if (hour(now()) %in% 20:24){
+if (hour(now()) == 22){
   Sys.sleep(30)
   tweet(dailyActivity(1))
 }
-if (!is.null(getBirthdayNotification(birthdays))){
+if (!is.null(getBirthdayNotification(birthdays)) && hour(now()) == 20){
   Sys.sleep(60)
   tweet(getBirthdayNotification(birthdays))
 }
