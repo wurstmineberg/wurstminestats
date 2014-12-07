4ab3402b00a3f95cd7fe11a45853482db21e6efe
jemus42 <jemus42@quantenbrot.de>
2014-12-07 22:54:37 +0000

    Okay, bot _now_ it should work

--- dataPrep.R
+++ dataPrep.R
@@ -112,9 +112,6 @@ avgPerMonth      <- mean(ddply(playedPerMonth, .(month), summarize, timePlayed=s
 playtime.people  <- ddply(playedPerPerson, "person", summarize, timePlayed=sum(timePlayed))
 
 # Fix playerSession person names
-for(i in playerSessions$person){
-  playerSessions$person[playerSessions$person == i] <- people$name[people$id == i]
-}; rm(i)
 
 playerSessions$person <- factor(playerSessions$person, levels = people$id, ordered = T)
 
