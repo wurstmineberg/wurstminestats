bd2b1b52d7c5360355ffcf271e37a92b02894bd3
jemus42 <jemus42@quantenbrot.de>
2015-09-02 02:14:20 +0200

    Move stuff around

--- config/options.R
+++ config/options.R
@@ -3,23 +3,8 @@
 #### Data sources ####
 message("Setting global options")
 ## Strings & other descriptive JSON stuff ##
-options(url.strings.general         = "http://assets.wurstmineberg.de/json/strings.json")
-options(url.strings.mobs            = "http://assets.wurstmineberg.de/json/mobs.json")
-options(url.strings.achievements    = "http://assets.wurstmineberg.de/json/achievements.json")
-options(url.strings.items           = "http://assets.wurstmineberg.de/json/items.json")
-options(url.strings.biomes          = "http://assets.wurstmineberg.de/json/biomes.json")
-# In contrast to the above files, the next one is server specific, because people.
-options(url.strings.people          = "http://wurstmineberg.de/assets/serverstatus/people.json")
 
-## API-URLs ##
-options(url.stats.general           = "http://api.wurstmineberg.de/server/playerstats/general.json")
-options(url.stats.achievements      = "http://api.wurstmineberg.de/server/playerstats/achievement.json")
-options(url.stats.entities          = "http://api.wurstmineberg.de/server/playerstats/entity.json")
-options(url.stats.items             = "http://api.wurstmineberg.de/server/playerstats/item.json")
-
-options(url.general.deaths.latest   = "http://api.wurstmineberg.de/server/deaths/latest.json")
-options(url.general.deaths          = "http://api.wurstmineberg.de/server/deaths/overview.json")
-options(url.general.sessions        = "http://api.wurstmineberg.de/server/sessions/overview.json")
+urls <- jsonlite::fromJSON("urls.json")
 
 #### Dependencies ####
 message("Loading required packages")
@@ -31,15 +16,14 @@ library("RColorBrewer", quietly = TRUE)     # Because colours
 library("lubridate", quietly = TRUE)        # Time is hell, people.
 library("jsonlite", quietly = TRUE)         # Because JSON. I don't know if RJSONIO or rjson would wourk, too. Sorry.
 library("stringr", quietly = TRUE)          # Only used for the random stat functions
-library("twitteR", quietly = TRUE)          # Used to tweet said random stats, not required for plotting etc.
-library("plyr", quietly = TRUE)             # Data transformations are hard.
+#library("plyr", quietly = TRUE)             # Data transformations are hard.
 library("dplyr", quietly = TRUE)            # ^
 
 ##### Directories for plot locations ####
 message("Checking if plot directories are present")
-plotdirs <- c("Plots", "Plots/items", "Plots/items/mined", "Plots/items/crafted", "Plots/items/broken",
-              "Plots/items/used", "Plots/achievements", "Plots/statspage", "Plots/statspage/scaled",
-              "Plots/sessions")
+plotdirs <- c("output", "output/items", "output/items/mined", "output/items/crafted", "output/items/broken",
+              "output/items/used", "output/achievements", "output/statspage", "output/statspage/scaled",
+              "output/sessions")
 
 for (i in plotdirs){
   if (!file.exists(i)){
@@ -58,7 +42,7 @@ playerTheme <- theme(legend.position = "right",
 
 plotWidth <- 6; plotHeight <- 5;
 barChart  <- geom_bar(colour = "black", width = .7, stat = "identity")
-xLable    <- xlab("Player")
+xLable    <- xlab("Person")
 
 # Define colour scale to keep status/people colours static
 statusColours       <- brewer.pal(9,"Set1")
